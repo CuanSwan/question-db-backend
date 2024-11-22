c8bb8412a0e8b062659311119a25adccd6fab568
CuanSwan <cuan@itonlinelearning.com>
2024-11-22 09:36:07 +0000

    removed console.logs for token and passmatch

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,16 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 import User from '../models/Users.js';
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt';
 
 
+ 
 export const userLogin = async (req, res) => {
     const { email, password } = req.body;
     const userExists = await User.findOne({email})
     const passMatch = await bcrypt.compare(password, userExists.password)
-    console.log(passMatch)
     if(userExists && passMatch){
         console.log('UserFound')
         const token = jwt.sign({email}, process.env.SECRET, {expiresIn: "5h"})
-        console.log(token)
         res.status(200).json(token)
     }else{
         res.send('Email or password incorrect.')
@@ -28,25 +27,16 @@ export const userRegister = async (req, res) => {
         console.log(userExists)
         res.status(409).json('User already exists');
     }else{
-        user.createdAt = new Date(Date.now()).toUTCString();
+        user.createdAt = new Date.toUTCString();
         user.progress = 0;
         user.token = "";
         user.password = await bcrypt.hash(user.password, 10)
         console.log(user);
         try{
-            User.create(user)
+            await User.create(user)
             res.status(200).send("OK");
         }catch (err){
             console.error(err)
-        }
-        
+        } 
     }
 };
-    //     name: String,
-    //     surname: String,
-    //     email: String,
-    //     courses: Object,
-    //     progress: Object,
-    //     password: String,
-    //     createdAt: {type: Date, default: Date.now},
-    //     token: String
