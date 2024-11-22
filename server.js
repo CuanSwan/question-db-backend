639fdcc12c6d4a523387946d312db058de13c38c
CuanSwan <cuan@itonlinelearning.com>
2024-11-22 09:33:03 +0000

    Removed redundant declarations

--- server.js
+++ server.js
@@ -5,17 +5,16 @@ import 'dotenv/config'
 import getquestion from './controllers/getquestion.js';
 import {createCourse,addLessons,getCourses} from './controllers/CourseController.js';
 import { userLogin, userRegister } from './controllers/userController.js';
+import morgan from 'morgan';
 
 const app = express();
 const PORT = process.env.PORT;
  
 app.use(cors());
 app.use(json());
+app.use(morgan(':method :url :status :response-time ms - :res[content-length]'));
  
-mongoose.connect("mongodb://localhost:27017/courses", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect("mongodb://localhost:27017/courses")
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -26,11 +25,12 @@ db.once('open', function(){
 
 
 import exam  from './routes/exams.js';
+import { verifyToken } from './middleware/verifyToken.js';
 app.get('/api/exams', exam);
-app.use('/user/login', userLogin);
-app.use('/user/register', userRegister);
-app.get('/python/:course/:quesnum', getquestion);
-app.get('/courses/:courseCat', getCourses)
+app.post('/user/login', userLogin);
+app.post('/user/register', userRegister);
+app.get('/python/:quesnum', getquestion);
+app.get('/courses/:courseCat',verifyToken, getCourses)
 app.post('/admin/save/lessons', addLessons)
 app.post('/admin/create/course', createCourse)
 
